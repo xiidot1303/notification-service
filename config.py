15f35bda452be6568841e7c1cb65908745287a6b
xiidot1303 <xii1303@inbox.ru>
2022-08-12 18:29:42 +0500

    add secret key in config

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ from pathlib import Path
 load_dotenv(os.path.join('.env'))
 
 TOKEN=os.environ.get('TOKEN')
+SECRET_KEY=os.environ.get('SECRET_KEY')
--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from config import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$lqw4tn77vkgrrx+6u)@p*m#&(b-6+rlt8!g9@@i1kx&0=_q#z'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
